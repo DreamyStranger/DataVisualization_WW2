5914dfcfe2069dbb07dbc637408894b2b36812f9
DreamyStranger <98614364+DreamyStranger@users.noreply.github.com>
2023-05-09 03:37:59 -0400

    effects + refactoring + cross platform

--- bar_chart.js
+++ bar_chart.js
@@ -2,30 +2,15 @@ import { createPieChart } from './pie_chart.js';
 
 export function createBarChart(country, data, color) {
 
-    //remove old graph and tooltip
-    d3.select("#center-graph svg").remove();
-    d3.select("#tooltip").remove();
-    
     // Update the title and subtitle
-    d3.select("h1").text(`WW2 Casualties - ${country}`);
-    d3.select("h2").text("Military vs Civilian Casualties");
+    Initialize(country);
 
     // Get data for the selected country
     const selectedCountry = data.filter(d => d.Country === country)[0];
-    const dataForCountry = [
-        { type: "Military", value: selectedCountry.Military_Casualties },
-        { type: "Civilian", value: selectedCountry.Civilian_Casualties }
-    ];
+    const dataForCountry = [{ type: "Military", value: selectedCountry.Military_Casualties }, { type: "Civilian", value: selectedCountry.Civilian_Casualties }];
 
     // Define dimensions for the chart
-    const containerWidth = document.getElementById('center-graph').clientWidth - 20; // Subtract padding
-    const containerHeight = document.getElementById('center-graph').clientHeight - 20; // Subtract padding
-    const width = 0.8 * containerWidth;
-    const height = 0.8 * containerHeight;
-    const margin = { top: 10, right: 10, bottom: 10, left: 10 };
-    const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
-
+    const { containerWidth, containerHeight, width, height, innerWidth, innerHeight } = SetDimensions();
 
     // Set up scales for the chart
     const x = d3.scaleBand()
@@ -36,37 +21,79 @@ export function createBarChart(country, data, color) {
         .range([innerHeight, 0])
         .domain([0, d3.max(dataForCountry, d => d.value)]);
 
-        const svg = d3.select("#center-graph")
+    const svg = d3.select("#center-graph")
         .append("svg")
         .attr("width", containerWidth)
         .attr("height", containerHeight)
         .attr("viewBox", `0 0 ${containerWidth} ${containerHeight}`) // Add viewBox attribute
         .append("g")
-        .attr("transform", `translate(${(containerWidth - width) / 2 + margin.left}, ${(containerHeight - height) / 2 + margin.top})`);
+        .attr("transform", `translate(${(containerWidth - width) / 2}, ${(containerHeight - height) / 2})`);
 
     // Add rectangle elements for each data point
-    svg.selectAll("rect")
-        .data(dataForCountry)
-        .enter().append("rect")
-        .attr("x", d => x(d.type))
-        .attr("y", d => y(d.value))
-        .attr("width", x.bandwidth())
-        .attr("height", d => innerHeight - y(d.value))
-        .style("fill", d => color);
+    const bars = AddBars(svg, dataForCountry, x, y, color);
 
     // Add axes to the bar chart
     const xAxis = d3.axisBottom(x);
     const yAxis = d3.axisLeft(y);
+
+    AddXAxis(svg, innerHeight, xAxis);
+    AddYAxis(svg, yAxis);
+
+    FadeInText("h1", `WW2 Casualties - ${country}`, () => {
+        FadeInText("h2", `Can you guess an event?`)
+    })
+
+    FadeIn(bars, y, innerHeight);
+
+    // Create a button or clickable element to transition back to the pie chart
+    const backButton = AddBackButton(svg);
+
+    // Add click event listener to the back button to transition back to the pie chart
+    backButton.on("click", () => {
+        FadeOutText("h2", () => {
+            FadeOutText("h1")
+        });
+        FadeOut(bars, innerHeight, () => {
+            RemoveBars(bars);
+            createPieChart(data);
+        });
+    });
+}
+
+// HelperFunctions
+
+function Initialize(country) {
+    d3.select("#center-graph svg").remove();
+    d3.select("#tooltip").remove();
+    d3.select("h1").selectAll("tspan").remove();
+    d3.select("h2").selectAll("tspan").remove();
+}
+
+function SetDimensions() {
+    let containerWidth = document.getElementById('center-graph').clientWidth - 20; // Subtract padding
+    let containerHeight = document.getElementById('center-graph').clientHeight - 20; // Subtract padding
+    let width = 0.8 * containerWidth;
+    let height = 0.8 * containerHeight;
+    let innerWidth = width;
+    let innerHeight = height;
+    return { containerWidth, containerHeight, width, height, innerWidth, innerHeight };
+}
+
+function AddXAxis(svg, innerHeight, xAxis) {
     svg.append("g")
         .attr("transform", `translate(0, ${innerHeight})`)
         .call(xAxis);
+}
+
+function AddYAxis(svg, yAxis) {
     svg.append("g")
         .call(yAxis);
+}
 
-    // Create a button or clickable element to transition back to the pie chart
+function AddBackButton(svg) {
     const backButton = svg.append("text")
-        .attr("x", width - margin.right - 70)
-        .attr("y", margin.top)
+        .attr("x", 400)
+        .attr("y", 400)
         .attr("class", "back-button")
         .text("Back to Pie Chart")
         .style("fill", "black")
@@ -74,10 +101,88 @@ export function createBarChart(country, data, color) {
         .style("font-size", "14px")
         .style("cursor", "pointer");
 
-    // Add click event listener to the back button to transition back to the pie chart
-    backButton.on("click", () => {
-        d3.select("#center-graph svg").remove(); // Remove the bar chart
-        createPieChart(data); // Recreate the pie chart
+    return backButton;
+}
+
+function AddBars(svg, dataForCountry, x, y, color) {
+    return svg.selectAll("rect")
+        .data(dataForCountry)
+        .enter().append("rect")
+        .attr("x", d => x(d.type))
+        .attr("y", y(0)) // Set initial y value to y(0)
+        .attr("width", x.bandwidth())
+        .attr("height", 0) // Set initial height to 0
+        .style("fill", d => color);
+}
+
+
+function RemoveBars(bars) {
+    bars.transition()
+        .duration(1000)
+        .attr("height", 0)
+        .attr("y", d => innerHeight)
+        .style("opacity", 0)
+        .remove();
+}
+
+function FadeInText(selector, text, onEnd = null) {
+
+    const textDuration = 35; // duration for each letter's transition
+    let textSelection = d3.select(selector);
+    let lastIndex = text.length - 1;
+    textSelection.text("");
+
+    text.split("").forEach((letter, index) => {
+        textSelection.append("tspan")
+            .text(letter)
+            .style("opacity", 0)
+            .transition()
+            .delay(index * textDuration)
+            .duration(textDuration)
+            .style("opacity", 1)
+            .on("end", function () {
+                if (index === lastIndex && onEnd) {
+                    onEnd();
+                }
+            });
     });
 }
 
+
+function FadeOutText(selector, onEnd = null) {
+    const textDuration = 35; // duration for each letter's transition
+    let textSelection = d3.select(selector);
+    let tspans = textSelection.selectAll("tspan").nodes().reverse();
+
+    tspans.forEach((tspan, index) => {
+        d3.select(tspan)
+            .transition()
+            .delay(index * textDuration)
+            .duration(textDuration)
+            .style("opacity", 0)
+            .on("end", function () {
+                if (index === tspans.length - 1) {
+                    onEnd();
+                }
+            });
+    });
+}
+
+//Transition In
+function FadeIn(bars, y, innerHeight, callback = null) {
+    // Add a transition effect to make the bars grow in height gradually
+    bars.transition()
+        .duration(2300)
+        .attr("y", d => y(d.value))
+        .attr("height", d => innerHeight - y(d.value));
+}
+
+//Transition Out
+function FadeOut(bars, innerHeight, callback = null) {
+    bars.transition()
+        .duration(2300)
+        .ease(d3.easeCubicInOut)
+        .attr("height", 0)
+        .attr("y", d => innerHeight)
+        .on("end", callback);
+}
--- pie_chart.js
+++ pie_chart.js
@@ -1,45 +1,79 @@
 import { createBarChart } from './bar_chart.js';
 
 export function createPieChart(data) {
+    const TOUCH_THRESHOLD = Initialize();
 
-    //useful constants
-    let touched = 0;
-    const TOUCH_THRESHOLD = 300; // milliseconds
-    let touchStartTime = 0;
-    let touchEndTime = 0;
+    const { width, height, radius } = createPieChartDimensions();
+
+    const color = createColorScale(data);
+
+    const pie = createPieGenerator();
+
+    const arc = createArcGenerator(radius);
+
+    const { svg, tooltip } = createSvgAndTooltip(width, height);
+
+    const pieSlices = createPieSlices(svg, data, pie, arc, color);
+
+    FadeInText("h1", "WW2 Casualties", () => FadeInText("h2", "Can you guess a country by the color?"));
+    FadeIn(pieSlices);
+
+    handleTouchEvents(pieSlices, arc, tooltip, TOUCH_THRESHOLD, createBarChart, data, color, radius, pieSlices);
+}
 
 
-    const width = document.getElementById('center-graph').clientWidth;
-    const height = document.getElementById('center-graph').clientHeight;
-    const radius = 0.8 * Math.min(width, height) / 2;
+//Helper Functions
 
-    const color = d3.scaleOrdinal()
+function Initialize() {
+
+    d3.select("#center-graph svg").remove();
+    d3.select("#tooltip").remove();
+
+    let TOUCH_THRESHOLD = 500; // milliseconds
+    return TOUCH_THRESHOLD;
+}
+
+function createPieChartDimensions() {
+    let width = document.getElementById('center-graph').clientWidth;
+    let height = document.getElementById('center-graph').clientHeight;
+    let radius = 0.8 * Math.min(width, height) / 2;
+
+    return { width, height, radius };
+}
+
+function createColorScale(data) {
+    let color = d3.scaleOrdinal()
         .domain(data.map(d => d.Total_Casualties))
         .range(d3.schemeCategory10);
 
-    const sliceColors = data.map(d => color(d.Total_Casualties));
+    return color;
+}
 
-    const pie = d3.pie()
+function createPieGenerator() {
+    let pie = d3.pie()
         .value(d => d.Total_Casualties)
         .sort(null);
 
-    const arc = d3.arc()
+    return pie;
+}
+
+function createArcGenerator(radius) {
+    let arc = d3.arc()
         .innerRadius(0)
         .outerRadius(radius);
 
-    const svg = d3.select("#center-graph")
+    return arc;
+}
+
+function createSvgAndTooltip(width, height) {
+    let svg = d3.select("#center-graph")
         .append("svg")
         .attr("width", width)
         .attr("height", height)
         .append("g")
         .attr("transform", `translate(${width / 2}, ${height / 2})`);
 
-    const g = svg.selectAll(".arc")
-        .data(pie(data))
-        .enter().append("g")
-        .attr("class", "arc");
-
-    const tooltip = d3.select("body")
+    let tooltip = d3.select("body")
         .append("div")
         .attr("id", "tooltip")
         .attr("class", "tooltip");
@@ -54,13 +88,107 @@ export function createPieChart(data) {
     tooltip.append("div")
         .attr("id", "hint");
 
-    d3.select("h1").text("WW2 Casualties");
-    d3.select("h2").text("Can you guess a country by the color?");
+    return { svg, tooltip };
+}
 
+function createPieSlices(svg, data, pie, arc, color) {
+    let g = svg.selectAll(".arc")
+        .data(pie(data))
+        .enter().append("g")
+        .attr("class", "arc")
+        .style("opacity", 0); // Set initial opacity to 0
+
+    return g;
+}
+
+function FadeInText(selector, text, onEnd = null) {
+
+    const textDuration = 35; // duration for each letter's transition
+    let textSelection = d3.select(selector);
+    let lastIndex = text.length - 1;
+    textSelection.text("");
+
+    text.split("").forEach((letter, index) => {
+        textSelection.append("tspan")
+            .text(letter)
+            .style("opacity", 0)
+            .transition()
+            .delay(index * textDuration)
+            .duration(textDuration)
+            .style("opacity", 1)
+            .on("end", function () {
+                if (index === lastIndex && onEnd) {
+                    onEnd();
+                }
+            });
+    });
+}
+
+
+function FadeOutText(selector, onEnd = null) {
+    const textDuration = 35; // duration for each letter's transition
+    let textSelection = d3.select(selector);
+    let tspans = textSelection.selectAll("tspan").nodes().reverse();
+  
+    tspans.forEach((tspan, index) => {
+      d3.select(tspan)
+        .transition()
+        .delay(index * textDuration)
+        .duration(textDuration)
+        .style("opacity", 0)
+        .on("end", function () {
+          if (index === tspans.length - 1) {
+            onEnd();
+          }
+        });
+    });
+  }
+
+function FadeIn(pieSlices, onEnd = null) {
+    let sliceDuration = 200; // duration for each slice's transition
+
+    pieSlices.each(function (d, index) {
+        d3.select(this)
+            .transition()
+            .delay(index * sliceDuration)
+            .duration(sliceDuration)
+            .style("opacity", 1)
+            .on("end", function () {
+                if (index === pieSlices.size() - 1) {
+                    onEnd();
+                }
+            });
+    });
+}
+
+function FadeOut(pieSlices, onEnd = null) {
+    const sliceDuration = 200; // duration for each slice's transition
+
+    pieSlices.each(function (d, index) {
+        d3.select(this)
+            .transition()
+            .delay(index * sliceDuration)
+            .duration(sliceDuration)
+            .style("opacity", 0)
+            .on("end", function () {
+                if (index === pieSlices.size() - 1) {
+                    onEnd();
+                }
+            });
+    });
+}
+
+
+
+function handleTouchEvents(g, arc, tooltip, TOUCH_THRESHOLD, createBarChart, data, color, radius, pieSlices) {
+
+    let touchStartTime = 0;
+    let touchEndTime = 0;
     g.append("path")
         .attr("d", arc)
         .style("fill", d => color(d.data.Total_Casualties))
         .on("mouseenter touchstart", function (event, d) {
+
             touchStartTime = new Date().getTime();
             tooltip.select("#country").text(d.data.Country);
             tooltip.select("#total-casualties").text(`Total casualties: ${d.data.Total_Casualties.toLocaleString()}`);
@@ -78,10 +206,12 @@ export function createPieChart(data) {
                     .innerRadius(0)
                     .outerRadius(radius * 1.1)
                 );
+
         })
         .on("mousemove touchmove", function (event) {
             tooltip.style("left", (event.pageX + 10) + "px")
                 .style("top", (event.pageY - 28) + "px");
+
         })
         .on("mouseleave touchend", function () {
             tooltip.style("left", "9999px");
@@ -91,21 +221,26 @@ export function createPieChart(data) {
                 .transition()
                 .duration(200)
                 .attr("d", arc);
-            touched = 0;
         })
         .on("click touchend", function (event, d) {
+            event.stopPropagation(); // Prevent triggering other touchend listeners
             touchEndTime = new Date().getTime();
             let touchDuration = touchEndTime - touchStartTime;
             if (touchDuration > TOUCH_THRESHOLD) {
-                event.stopPropagation(); // Prevent triggering other touchend listeners
-                    createBarChart(d.data.Country, data, sliceColors[d.index]);
+                FadeOutText("h2", () => {
+                    FadeOutText("h1")
+                });
+                FadeOut(pieSlices, () => {
+                    createBarChart(d.data.Country, data, color(d.data.Total_Casualties));
+                });
             }
+            touchStartTime = new Date().getTime();
+
         });
 
     // Close tooltip on touchend event outside the pie chart
     d3.select("body")
         .on("touchend", function (event) {
-            // Hide tooltip on touchend event outside the pie chart
             if (!event.target.closest(".arc")) {
                 tooltip.style("left", "9999px");
 
@@ -119,3 +254,4 @@ export function createPieChart(data) {
             }
         });
 }
+
